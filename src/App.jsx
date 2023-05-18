9ec1cf12d31ced4d56d9d013d5275fdae9e905be
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-05-18 20:27:11 +0100

    Sets up routes

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Dashboard from "./pages/Dashboard/Dashboard.jsx"
+import TicketPage from './pages/TicketPage/TicketPage';
+import { Routes, Route } from "react-router-dom";
+import Form from "./pages/Form/Form";
+import Navbar from "./components/Navbar/Navbar.jsx"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path ="/" element={<Form />} />
+        <Route path="/tickets" element={<Dashboard/>} />
+        <Route path="/tickets/:id" element={<TicketPage/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
