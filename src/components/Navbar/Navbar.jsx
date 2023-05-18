113ae10bfb1805446117b1fa7d1116e0e6172ddc
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-05-18 22:19:07 +0100

    Connected ticket page with dashboard and database

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import sampleLogo from "../../images/Logo.png";
+import "./Navbar.css";
 
 export default function Navbar () {
     return (
         <div className="navbar-container">
-            <img src={sampleLogo} alt="logo"/>
-            <ul>
+            <img className="logo-navbar" src={sampleLogo} alt="logo"/>
+            <ul className="navbar-list">
                 <li>
-                    <Link to="/">Register ticket</Link>
+                    <Link to="/" className="navbar-item">Register ticket</Link>
                 </li>
-                <li>
-                    <Link to="/tickets">Dashboard</Link>
+                <li >
+                    <Link to="/tickets" className="navbar-item">Dashboard</Link>
                 </li>
             </ul>
         </div>
