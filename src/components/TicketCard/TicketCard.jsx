f8c8fbe82543d2ab56726e9169d9ca349823ba5b
claudiaalmeidavs <claudiaalmeida.traducao@gmail.com>
2023-05-19 11:37:06 +0100

    Adds css style to ticket card

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./TicketCard.css"
 
 
 export default function TicketCard ({ticket, ticketId, updateStatus}) {
@@ -11,12 +12,13 @@ export default function TicketCard ({ticket, ticketId, updateStatus}) {
         {ticket ? (
             <div className="inner-card-container">
                 <h3 className="card-heading">Ticket #{ticket.id}</h3>
-                <p className="card-text">Priority: {ticket.ticket_priority}</p>
-                <p className="card-text">Subject: {ticket.ticket_subject}</p>     
+                <p><strong>Requested by user id</strong>: {ticket.user_id}</p>
+                <p className="card-text"><strong>Priority</strong>: {ticket.ticket_priority}</p>
+                <p className="card-text"><strong>Subject:</strong> {ticket.ticket_subject}</p>     
                 <form className="center">
-                  <label htmlFor="change-status">
-                    Change status{" "}
-                      <select id="status-select" onChange={handleChange}>
+                  <label htmlFor="change-status"><strong>
+                    Change status</strong>{" "}
+                      <select className="status-select" onChange={handleChange}>
                         <option value="">---</option>
                         <option value="To Do">To Do</option>
                         <option value="In Progress">In Progress</option>
@@ -24,7 +26,8 @@ export default function TicketCard ({ticket, ticketId, updateStatus}) {
                       </select>
                   </label>
                 </form>
-                <p className="card-text"><Link to={`/tickets/${ticketId}`} className="see-ticket-link">See ticket</Link></p>
+                <br />
+                <p className="card-text link-text"><Link to={`/tickets/${ticketId}`} className="see-ticket-link">View ticket</Link></p>
             </div>
         ) : null}
         </div>
